d44110a1ce7a2fcf2344ed92791382e916596dda
Jakub Czadankiewicz <jakub.czadankiewicz@gmail.com>
2018-12-09 11:54:32 +0100

    update to movie service

--- src/main/java/pl/sda/rental/movies/model/MovieBuilderService.java
+++ src/main/java/pl/sda/rental/movies/model/MovieBuilderService.java
@@ -4,6 +4,9 @@ package pl.sda.rental.movies.model;
 import lombok.AllArgsConstructor;
 import pl.sda.rental.movies.dto.MovieDto;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @AllArgsConstructor
 public class MovieBuilderService {
 
@@ -17,6 +20,16 @@ public class MovieBuilderService {
         result.setProductionDate(movieDto.getProductionDate());
         result.setLength(movieDto.getLength());
         result.setId(Long.valueOf(movieDto.getId()));
+
+        List<Person> directors = movieDto.getDirectors().stream()
+                .map(x -> personBuilderService.entityFromDto(x))
+                .collect(Collectors.toList());
+
+        List<Person> cast = movieDto.getCast().stream()
+                .map(x -> personBuilderService.entityFromDto())
+                .collect(Collectors.toList());
+        result.setDirectors(directors);
+        result.setCast(cast);
 
         return result;
     }
